388ac6d54119f2d9807b068447bf3fd42c761ed7
최지안 <etri33086@etri.re.kr>
2024-07-24 09:43:32 +0900

    feat / rename : module.scss로 변경, 전역 상태 Zustand 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.module.scss'; // 연결안됨
+import styles from './App.module.scss';
+
+import Test from './Test/Test';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
+
+
+  // ** =============뷰포트 높이 구하기 ===============** //
+  
+    /** 현재 뷰포트 높이의 1%를 계산한다. */
+    const setVh = (): void => {
+      const vh:number = window.innerHeight * 0.01; //window.innerHeight : 뷰포트의 높이를 가져온다. | 뷰포트 높이를 100분의 1로 나누어서 사용하는 것 (1%)
+      document.documentElement.style.setProperty('--vh', `${vh}px`); // css 변수를 만들어준다.
+    }
+
+    // * 첫 렌더링시, 뷰포트 사이즈 계산 후 적용
+    useEffect(()=> {
+      setVh();
+      
+      // 사이즈가 변경될 때, 다시 뷰포트 높이를 구한다.
+      const onResize = ():void => {
+        setVh();
+      }
+                              // 이벤트 유형 , 이벤트가 발생할때 실행하는 함수.
+      window.addEventListener('resize', onResize);
+    },[]);
+
+
+  // ** ============================================** //
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.screen}>
+      <Routes>
+        <Route path="/" element = {<div >대시보드 </div>}/>
+        <Route path="/structure" element={<div>구조화면</div>} />
+
+        {/* SMO/OAM */}
+        <Route path="/smo-oam" element={<div>smo-oam</div>} />
+
+        {/* Non-RT */}
+        <Route path="rapp" element={<div>rapp</div>} />
+        <Route path="framework" element={<div>framework</div>} />
+
+        {/* Near-RT */}
+        <Route path="xapp" element={<div>xapp</div>} />
+        <Route path="platform" element={<div>platform</div>} />
+
+        {/* E2 Node */}
+        <Route path="e2-node" element={<div>e2-node</div>} />
+
+        </Routes>
     </div>
   );
 }
---
+++ src/Test/Test.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import useTestStore from "../store/useTestStore";
+
+const Test: React.FC = () => {
+
+    const {count, increment, decrement } = useTestStore();
+
+    return(
+        <div>
+            <h2>Count : {count} </h2>
+            <button onClick={increment}> Increment </button>
+            <button onClick={decrement}> decrement </button>
+        </div>
+    )
+}
+export default Test;
+
+
+//* props.를 사용하는 경우
+// interface TestProps {
+//     message: string;
+//   }
+  
+//   const Test: React.FC<TestProps> = ({ message }) => {
+//     return (
+//       <div>
+//         {message}
+//       </div>
+//     );
+//   }
+
+
+//* 상태를 사용하는 경우
+
+// const Test: React.FC = () => {
+//     const [count, setCount] = useState<number>(0);
+  
+//     return (
+//       <div>
+//         <p>Count: {count}</p>
+//         <button onClick={() => setCount(count + 1)}>Increment</button>
+//         <button onClick={() => setCount(count - 1)}>Decrement</button>
+//       </div>
+//     );
+//   }
